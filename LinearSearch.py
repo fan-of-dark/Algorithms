60d9c4a63612095ead8a9e8077c5a2e940677f7f
SAI VIKAS SINGAMSETTY <66284862+fan-of-dark@users.noreply.github.com>
2021-03-01 12:11:35 +0530

    Delete LinearSearch.py

--- LinearSearch.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-lst = list(map(int,input().split()))
-key = int(input())
-for i in range(len(lst)-1,-1,-1):
-    if lst[i] == key:
-        print(i)
-        break
-else: print("No")
